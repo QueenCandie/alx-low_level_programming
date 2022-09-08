8054cb777314031f3777883b31881452f7313ebd
“Obarayo <seyiobarayobarayo@gmail.com>
2022-09-08 10:50:07 -0700

    Task 4

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,13 +1,10 @@
-#!/bin/bash
-#include <stdio.h>
-
+# include <stdio.h>
 /**
- * main - Entry point
- *
- * Return: Always 0 (Success)
+ * Description: mai - Prints out a string to stdout
+ * Return: 0 if success
  */
 int main(void)
 {
-        puts("\"Programming is like building a multilingual puzzle");
-        return (0);
-}	
+puts("\"Programming is like building a multilingual puzzle");
+return (0);
+}
