b4cf6573f07a7dbbdc91dde2a22f24f9af03523b
“Obarayo <seyiobarayobarayo@gmail.com>
2022-09-09 14:30:12 -0700

    Task 9

--- 0x01-variables_if_else_while/9-print_comb.c
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -1,23 +1,23 @@
 #include <stdio.h>
 /**
- * main - Entry point
- * Return: 0
+ * main - Prints all possible combinations of single-digit numbers
+ * Return: Always 0
  */
 int main(void)
 {
-	int i;
+	int num;
 
-	for (i = 48; i < 58; i++)
+	for (num = 0; num <= 9; num++)
 	{
-		putchar(i);
-		if (i != 57)
-		{
-			putchar(',');
-			putchar('');
-		}
+		putchar((num % 10) + '0');
+		if (num == 9)
+			continue;
+
+		putchar(',');
+		putchar(' ');
 	}
+
 	putchar('\n');
 	return (0);
 }
-
 
